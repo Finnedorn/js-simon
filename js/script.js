71415563181ec32d204f813442af4f1d84421c59
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-20 16:13:19 +0200

    added js html css files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Bonus :100: :
+Realizzare l'esercizio con grafica e senza utilizzo di prompt ma con casella/e  di input
+*/
+
+const numbersToGenerate = 5;
+const RngNumber = [];
+
+//creo un ciclo per ogni elemento da generare 
+while(RngNumber.length < numbersToGenerate) {
+    //se il numero è gia presente lo pusho nell'array
+};
+
+//mostro i numeri della pagina per 30 sec.
